5c23c9d36a89bb254df0802206d52d59a599b00b
Joe Monem <jmonem@icloud.com>
2023-10-03 10:38:52 +0300

    Initial Commit

---
+++ MoneyTimeConverter/MoneyTimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTimeConverterApp.swift
+//  MoneyTimeConverter
+//
+//  Created by Joe Monem on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
