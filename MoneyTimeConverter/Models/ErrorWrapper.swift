c194a741db5777de992f72e6c58d955560896d6c
Joe Monem <jmonem@icloud.com>
2023-10-05 10:41:54 +0300

    Second commit

---
+++ MoneyTimeConverter/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  MoneyTimeConverter
+//
+//  Created by Joe Monem on 04/10/2023.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
